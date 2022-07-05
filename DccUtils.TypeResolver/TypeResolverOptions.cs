6273f24b2145447cbf83170bf1fa3cb2dc5468d0
Kirill Dubinkin <programming.weapon@gmail.com>
2022-07-05 23:06:27 +0300

    Fixed TypeResolver's types loading issues with default configuration

--- DccUtils.TypeResolver/TypeResolverOptions.cs
+++ DccUtils.TypeResolver/TypeResolverOptions.cs
@@ -2,10 +2,10 @@ using Dcc.Reflection.TypeFormatting;
 
 namespace Dcc.Reflection.TypeResolver;
 
-public class TypeResolverOptions  {
+public class TypeResolverOptions {
     public TypeNameFormatter TypeNameFormatter { get; set; } = new TypeShortNameFormatter();
-    public Func<string, bool> AssemblyExclude { get; set; } = _ => false;
-    public Func<string, bool> AssemblyInclude { get; set; } = _ => true;
+    public Func<string, bool> AssemblyExclude { get; set; } = name => name.Contains("EntityFrameworkCore") && name.Contains("Design");
+    public Func<string, bool> AssemblyInclude { get; set; } = _ => false;
     public Func<Type, bool> TypeExclude { get; set; } = _ => false;
     public bool ThrowOnUnresolvedNameConflicts { get; set; }
     public Func<IEnumerable<string>>? AssemblyNamesResolver { get; set; }
